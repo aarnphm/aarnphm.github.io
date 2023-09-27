f3a72a0862ca26097a5f726817d22deb8b889f41
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-09-27 04:31:30 -0400

    chore: add a3 assignments code
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- content/dump/university/concurrent-design-3bb4/a3/Quicksort.java
+++ content/dump/university/concurrent-design-3bb4/a3/Quicksort.java
@@ -1,27 +1,21 @@
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Quicksort {
     static int N;   // number of elements to be sorted
     static int S;   // threshold for creating a sub-thread
     static int a[]; // array to be sorted
-    static ExecutorService threadPool;
+    static int activeThreads = 0;
 
     static int partition(int p, int r) {
         int x = a[r];
         int i = p - 1;
         for (int j = p; j <= r - 1; j++) {
             if (a[j] <= x) {
-                i++;
-                int t = a[i];
-                a[i] = a[j];
-                a[j] = t;
+                i++; 
+                int t = a[i]; a[i] = a[j]; a[j] = t;
             }
         }
-        int t = a[i + 1];
-        a[i + 1] = a[r];
-        a[r] = t;
+        int t = a[i + 1]; a[i + 1] = a[r]; a[r] = t;
         return i + 1;
     }
 
@@ -35,18 +29,28 @@ public class Quicksort {
 
     static void parallelsort(int p, int r) {
         if (p < r) {
-            if (r - p + 1 <= S) {
-                sequentialsort(p, r);
-            } else {
-                int q = partition(p, r);
-
-                if (threadPool == null) {
-                    // Create a thread pool with a fixed number of threads (e.g., equal to the number of CPU cores)
-                    threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            int q = partition(p, r);
+            int left = q - p;
+            int right = r - q;
+    
+            if (left > S && right > S && activeThreads < S) {
+                Thread rightThread = new Thread(() -> {
+                    activeThreads++;
+                    parallelsort(q + 1, r);
+                    activeThreads--;
+                });
+    
+                rightThread.start();
+                parallelsort(p, q - 1);
+    
+                try {
+                    rightThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-
-                threadPool.execute(() -> parallelsort(p, q - 1));
-                threadPool.execute(() -> parallelsort(q + 1, r));
+            } else {
+                sequentialsort(p, q - 1);
+                sequentialsort(q + 1, r);
             }
         }
     }
@@ -60,18 +64,8 @@ public class Quicksort {
 
         final long start = System.currentTimeMillis();
         parallelsort(0, N - 1);
-
-        if (threadPool != null) {
-            threadPool.shutdown();
-            try {
-                threadPool.awaitTermination(Long.MAX_VALUE, java.util.concurrent.TimeUnit.NANOSECONDS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
         final long end = System.currentTimeMillis();
-
+        
         for (int i = 1; i < N; i++) assert a[i - 1] <= a[i];
         System.out.println((end - start) + " ms");
     }
