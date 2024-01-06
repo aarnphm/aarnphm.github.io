d18ae9303a5f32f9dcee0f5a9f45114709aa8b52
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-01-06 17:15:34 -0500

    infra: update sync script
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 cd "$GIT_ROOT" || exit 1
 
 if [[ ! -d quartz.git ]]; then
-  gh repo clone jackyzha0/quartz --bare
+  gh repo clone jackyzha0/quartz -- --bare
 fi
 
 git --git-dir=quartz.git --work-tree=. checkout HEAD -- quartz
