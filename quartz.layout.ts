172fe0aa7ba522aa96e08ac75d2941b9559df87f
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-01-21 23:10:10 -0500

    fix(tools): run prettier correctly
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -61,7 +61,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
   ],
   right: [
-    Component.Graph({ localGraph: { repelForce: 0.79, centerForce: 0.2, scale: 1.04, linkDistance: 40 } }),
+    Component.Graph({
+      localGraph: { repelForce: 0.79, centerForce: 0.2, scale: 1.04, linkDistance: 40 },
+    }),
     Component.Backlinks(),
   ],
 }
