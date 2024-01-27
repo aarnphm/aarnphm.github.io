0c5fda13055d3f691d0979ec3a6cc638c917dc57
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-01-26 22:39:30 -0500

    fix: using edge runtime
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- api/curius.ts
+++ api/curius.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
 
 export const config = {
-  runtime: "nodejs",
+  runtime: "edge",
 }
 
 export default async function handler(req: VercelRequest, resp: VercelResponse) {
