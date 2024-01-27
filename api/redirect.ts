812a9c13d25eba6ce69a8c61e5be50cad195f225
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-01-27 12:31:02 -0500

    fix: remove redirection for recursive call
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- api/redirect.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node"
-
-export default async function handler(req: VercelRequest, resp: VercelResponse) {
-  const { subdomain, path } = req.query
-  const redirectUrl = `https://endpoints.aarnphm.xyz/api/raw?slug=${subdomain}/${path}`
-  resp.writeHead(307, { Location: redirectUrl })
-  resp.end()
-}
