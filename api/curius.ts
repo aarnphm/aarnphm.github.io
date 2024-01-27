80cbaaab467e0e9ee2d2e616c6c5525b7a3c843b
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-01-26 20:29:48 -0500

    feat(serverless): crawling all data
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- api/curius.ts
+++ api/curius.ts
@@ -1,14 +1,26 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
 
 export default async function handler(req: VercelRequest, resp: VercelResponse) {
-  const apiUrl = "https://curius.app/api/users/3584/links?page=0"
+  const baseUrl = "https://curius.app/api/users/3584/links"
+  let allLinks: any[] = []
+  let page = 0
+  let lastPage = false
 
   try {
-    const response = await fetch(apiUrl, {
-      headers: { "Content-Type": "application/json" },
-    })
-    resp.status(200).json(await response.json())
+    while (!lastPage) {
+      const response = await fetch(`${baseUrl}?page=${page}`, {
+        headers: { "Content-Type": "application/json" },
+      })
+      const data = await response.json()
+      if (data.userSaved && data.userSaved.length > 0) {
+        allLinks = [...allLinks, ...data.userSaved]
+        page++
+      } else {
+        lastPage = true
+      }
+    }
+    resp.status(200).json({ userSaved: allLinks })
   } catch (error: any) {
-    resp.status(500).json({ error: error.message })
+    resp.status(500).json({ error: error.message || "An error occurred while fetching data." })
   }
 }
