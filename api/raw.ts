5508fc3a56a8ce528dec740b3dd9e1fa093daaf3
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-01-27 09:31:57 -0500

    feat: add /api/raw for settings hosting raw files
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

---
+++ api/raw.ts
@@ -0,0 +1,29 @@
+import path from "path"
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+
+export default async function handler(req: VercelRequest, resp: VercelResponse) {
+  // Constructing the base URL
+  const protocol = req.headers["x-forwarded-proto"] || "http"
+  const host = req.headers["x-forwarded-host"] || req.headers.host
+  const baseUrl = `${protocol}://${host}`
+  const { path: slug } = req.query
+  try {
+    if (typeof slug !== "string") {
+      resp.status(400).send({ error: "given query is not a string" })
+    }
+
+    const staticUrl = path.join(baseUrl, typeof slug !== "string" ? [...slug].join("/") : slug)
+    console.log(staticUrl)
+
+    const data = await fetch(staticUrl)
+      .then((res) => res.text())
+      .catch((e) => {
+        resp.status(500).send({ error: e.message })
+      })
+
+    resp.setHeader("Content-Type", "text/plain")
+    resp.status(200).send({ content: data })
+  } catch (e: any) {
+    resp.status(500).send({ error: e.message })
+  }
+}
