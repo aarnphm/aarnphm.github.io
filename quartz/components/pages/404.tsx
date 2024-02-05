f1542473c2aeae85ae301b0244510d82decc5bbb
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-02-05 02:25:25 -0500

    revert: 404 changes
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,22 +1,13 @@
 import { i18n } from "../../i18n"
 import { QuartzComponentConstructor, QuartzComponentProps } from "../types"
-import styles from "../styles/404.scss"
-//@ts-ignore
-import notFoundScript from "../scripts/404.inline"
 
 function NotFound({ cfg }: QuartzComponentProps) {
   return (
-    <div class="not-found">
-      <article class="popover-hint">
-        <h1>404</h1>
-        <p>{i18n(cfg.locale).pages.error.notFound}</p>
-        <div id="typewritter"></div>
-      </article>
-    </div>
+    <article class="popover-hint">
+      <h1>404</h1>
+      <p>{i18n(cfg.locale).pages.error.notFound}</p>
+    </article>
   )
 }
-
-NotFound.css = styles
-NotFound.afterDOMLoaded = notFoundScript
 
 export default (() => NotFound) satisfies QuartzComponentConstructor
