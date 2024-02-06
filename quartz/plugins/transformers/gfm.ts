5a422a3063cce492cebed570aa0110a1c5e84719
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-02-06 01:06:53 -0500

    fix(search): set background-color for icon within preview panel
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz/plugins/transformers/gfm.ts
+++ quartz/plugins/transformers/gfm.ts
@@ -37,6 +37,7 @@ export const GitHubFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options> | 
                 ariaHidden: true,
                 tabIndex: -1,
                 "data-no-popover": true,
+                "data-icon": true,
               },
               content: {
                 type: "element",
