dd6579a424192f30cb65d72ccae08249807585ae
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-02-21 17:34:56 -0500

    chore: move up toc and remove recent notes
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -106,16 +106,20 @@ const beforeBody = (
   return { beforeBody }
 }
 
+const enableExplorer = true
+const enableMeta = true
+const enableRecentNotes = true
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   ...beforeBody(),
-  ...left({ enableRecentNotes: true }),
+  ...left(),
   ...right(),
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   ...beforeBody(false, false, false),
-  ...left({ enableExplorer: true, enableMeta: true }),
+  ...left({ enableExplorer, enableMeta }),
   right: [],
 }
