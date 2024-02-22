eed55ea2dc6c41c368022b97d14a2a4c217e8bf4
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-02-22 10:55:06 -0500

    chore: passing additional buildCtx to componentData
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz/plugins/emitters/contentPage.tsx
+++ quartz/plugins/emitters/contentPage.tsx
@@ -118,6 +118,7 @@ export const ContentPage: QuartzEmitterPlugin<Partial<FullPageLayout>> = (userOp
 
         const externalResources = pageResources(pathToRoot(slug), resources)
         const componentData: QuartzComponentProps = {
+          ctx,
           fileData: file.data,
           externalResources,
           cfg,
