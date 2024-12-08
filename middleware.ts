7a2cff8bc85badba1a91ad013548559a413a6879
Aaron Pham <contact@aarnphm.xyz>
2024-12-08 17:13:04 -0500

    fix(middleware): oops forgot to export default
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,15 @@
-import { rewrite } from "@vercel/edge"
+import { rewrite, next } from "@vercel/edge"
 
-export function middleware(request: Request) {
+export const config = {
+  matcher: ["/:path*"],
+}
+
+export default function middleware(request: Request) {
   const url = new URL(request.url)
-  // Only proceed if we're on notes.aarnphm.xyz
   console.log(url)
-  if (url.hostname.startsWith("notes.aarnphm.xyz")) {
-    return rewrite(new URL("/notes?stackedNotes=bm90ZXM", request.url))
+  // Only proceed if we're on notes.aarnphm.xyz
+  if (!url.hostname.startsWith("notes.aarnphm.xyz")) {
+    return next()
   }
+  return rewrite(new URL("/notes?stackedNotes=bm90ZXM", request.url))
 }
