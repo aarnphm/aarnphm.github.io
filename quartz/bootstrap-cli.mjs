c32a9bca9f409a5084152842e4b3be1227ec314c
Aaron Pham <contact@aarnphm.xyz>
2025-01-04 14:20:03 -0800

    chore: simplify cli
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- quartz/bootstrap-cli.mjs
+++ quartz/bootstrap-cli.mjs
@@ -1,26 +1,14 @@
 #!/usr/bin/env node
 import yargs from "yargs"
 import { hideBin } from "yargs/helpers"
-import {
-  handleBuild,
-  handleCreate,
-  handleUpdate,
-  handleRestore,
-  handleSync,
-} from "./cli/handlers.js"
-import { CommonArgv, BuildArgv, CreateArgv, SyncArgv } from "./cli/args.js"
+import { handleBuild, handleRestore, handleSync } from "./cli/handlers.js"
+import { CommonArgv, BuildArgv, SyncArgv } from "./cli/args.js"
 import { version } from "./cli/constants.js"
 
 yargs(hideBin(process.argv))
   .scriptName("quartz")
   .version(version)
   .usage("$0 <cmd> [args]")
-  .command("create", "Initialize Quartz", CreateArgv, async (argv) => {
-    await handleCreate(argv)
-  })
-  .command("update", "Get the latest Quartz updates", CommonArgv, async (argv) => {
-    await handleUpdate(argv)
-  })
   .command(
     "restore",
     "Try to restore your content folder from the cache",
