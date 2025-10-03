b49afcdad4d91c00286bf4073584cf4ffc3e7c50
Aaron Pham <contact@aarnphm.xyz>
2025-10-03 07:40:14 -0400

    fix: center check
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- quartz/components/pages/ChannelContent.tsx
+++ quartz/components/pages/ChannelContent.tsx
@@ -134,9 +134,9 @@ export default (() => {
                         />
                       </svg>
                       <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
+                        width="15"
+                        height="15"
+                        viewBox="-2 -2 16 16"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                         class="check-icon"
