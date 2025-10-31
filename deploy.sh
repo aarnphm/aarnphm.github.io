23047dc2f7f5eb78d382b96c5dbf64b0d3f65e91
Aaron Pham <contact@aarnphm.xyz>
2025-10-31 10:32:17 -0400

    fix: more verbose debug logs and correctly set var on local machine
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-CF_PAGES=1 NODE_ENV=production pnpm exec quartz/bootstrap-cli.mjs build --concurrency 4 --bundleInfo --verbose
+NODE_ENV=production pnpm exec quartz/bootstrap-cli.mjs build --concurrency 8 --bundleInfo --verbose
 
 fd --glob "*.pdf" public -x rm
 fd --glob "*.ddl" public -x rm
 
-pnpm wrangler deploy
+WRANGLER_LOG=debug pnpm wrangler deploy
