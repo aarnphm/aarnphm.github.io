ffb0777f0eb83c5059fbd4d69bfb7314a375a5b3
Aaron Pham <contact@aarnphm.xyz>
2025-11-03 10:55:44 -0500

    fix: include lfs checkout
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 NODE_ENV=production pnpm exec quartz/bootstrap-cli.mjs build --concurrency 10 --bundleInfo --verbose
 
-fd --glob "*.pdf" public -x rm
 fd --glob "*.ddl" public -x rm
 rm public/embeddings-text.jsonl
 
